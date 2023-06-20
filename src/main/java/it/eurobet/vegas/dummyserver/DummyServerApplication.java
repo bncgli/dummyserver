7b96131cdfec0bdcf5e25b2a0bbbadfc433e7971
GiulioBanchini <g.banchini@proximanetwork.it>
2023-06-20 17:28:12 +0200

    Added Spring Boot Admin server and index page

--- src/main/java/it/eurobet/vegas/dummyserver/DummyServerApplication.java
+++ src/main/java/it/eurobet/vegas/dummyserver/DummyServerApplication.java
@@ -1,9 +1,11 @@
 package it.eurobet.vegas.dummyserver;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class DummyServerApplication {
 
     public static void main(String[] args) {
